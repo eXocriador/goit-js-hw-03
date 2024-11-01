e4bc9614e8e4935c3ff0653aa89c8a562f6a20ea
Oleh Tatarynov <exocriador@gmail.com>
2024-11-01 21:47:36 +0200

    HW-03 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-    totalPrice = quantity*pricePerDroid
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+function slugify(title) {
+    title = title.toLowerCase();
+    title = title.split(" ");
+    title = title.join("-");
+    return title;
 }
 
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500)); 
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
